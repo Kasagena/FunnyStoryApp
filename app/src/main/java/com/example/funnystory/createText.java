f3799d3cf1764985b9311ee9697d890842377dd7
Kasagena <Kasagena@users.noreply.github.com>
2019-10-20 19:51:23 +0700

    Da hyle j blyat ne tak to syka
    No view found for id 0x7f07006c (com.example.funnystory:id/fragmentEbat) for fragment storyText{31fb8ea (d83b870e-a2c7-454f-9096-57a98ffffb6c) id=0x7f07006c}

--- app/src/main/java/com/example/funnystory/createText.java
+++ app/src/main/java/com/example/funnystory/createText.java
@@ -5,6 +5,8 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class createText extends AppCompatActivity {
@@ -14,14 +16,30 @@ public class createText extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_text);
-
-        FragmentManager fragmentmanager = getSupportFragmentManager();
-        FragmentTransaction fragmenttransaction = fragmentmanager.beginTransaction();
-        storyText storytext = new storyText();
-        fragmenttransaction.add(R.id.conteiner, storytext);
-        fragmenttransaction.commit();
+        addListenerOnButton();
     }
 
 
 
+    public void addListenerOnButton () {
+
+        Button createFragmentButton = findViewById(R.id.createStory_button);
+        createFragmentButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+//                        final EditText ob1 = findViewById(R.id.editText);
+//                        String text1 = ob1.getText().toString();
+//                        TextView storyText = findViewById(R.id.textStory);
+//                        storyText.setText(text1);
+
+                        storyText fragment111 = new storyText();
+                        FragmentManager fm = getSupportFragmentManager();
+                        FragmentTransaction ft = fm.beginTransaction();
+                        ft.add(R.id.fragmentEbat, fragment111);
+                        ft.commit();
+                    }
+                }
+        );
+    }
 }
--- app/src/main/java/com/example/funnystory/storyText.java
+++ app/src/main/java/com/example/funnystory/storyText.java
@@ -1,7 +1,5 @@
 package com.example.funnystory;
 
-import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,8 +7,6 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 
 public class storyText extends Fragment {
@@ -20,14 +16,7 @@ public class storyText extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        Context context = getActivity().getApplicationContext();
-        LinearLayout layout = new LinearLayout(context);
-        layout.setBackgroundColor(Color.parseColor("#EAF4FF"));
-        TextView text = new TextView(context);
-        text.setText("Xyipezda");
-        layout.addView(text);
-        return layout;
+        return inflater.inflate(R.layout.fragment_story_text,  container, false);
     }
 
 }
