f9e3e97e6b8e3ed3aa6443ce97f19c12035368ba
Kasagena <Kasagena@users.noreply.github.com>
2019-10-23 20:39:20 +0700

    Выводится текст "Hi"

--- app/src/main/java/com/example/funnystory/createText.java
+++ app/src/main/java/com/example/funnystory/createText.java
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.fragment.app.FragmentTransaction;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 
 public class createText extends AppCompatActivity {
@@ -18,7 +19,7 @@ public class createText extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_text);
     }
-    
+
     public void onButtonClick(View v) {
         LinearLayout ln = findViewById(R.id.linearLayout2);
         ln.setVisibility(View.INVISIBLE);
@@ -26,5 +27,30 @@ public class createText extends AppCompatActivity {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.add(R.id.fragment_story_text, sT);
         ft.commit();
+
+//        final EditText ob1 = findViewById(R.id.editText);
+//        String text1 = ob1.getText().toString();
+//        final EditText ob2 = findViewById(R.id.editText2);
+//        String text2 = ob2.getText().toString();
+//        final EditText ob3 = findViewById(R.id.editText3);
+//        String text3 = ob3.getText().toString();
+//        final EditText ob4 = findViewById(R.id.editText4);
+//        String text4 = ob4.getText().toString();
+//        final EditText ob5 = findViewById(R.id.editText5);
+//        String text5 = ob5.getText().toString();
+//        final EditText ob6 = findViewById(R.id.editText6);
+//        String text6 = ob6.getText().toString();
+//
+//        if (ob1.getText().length()*ob2.getText().length()*ob3.getText().length()*
+//                ob4.getText().length()*ob5.getText().length()*ob6.getText().length() == 0) {
+//            Toast.makeText(
+//                    createText.this, "Заполните все поля",
+//                    Toast.LENGTH_SHORT
+//            ).show(); }
+//        else {
+        Bundle bundle = new Bundle();
+        bundle.putString("text", "Hi" );
+        sT.setArguments(bundle);
+//            text1 + " " + text2 + " " + text3 + " " + text4 + " " + text5 + " " + text6
     }
 }
--- app/src/main/java/com/example/funnystory/storyText.java
+++ app/src/main/java/com/example/funnystory/storyText.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class storyText extends Fragment {
@@ -16,8 +17,11 @@ public class storyText extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_story_text, null);
-        return v;
+        View view = inflater.inflate(R.layout.fragment_story_text, null);
+
+        TextView textView = view.findViewById(R.id.textView7);
+        textView.setText(getArguments().getString("text"));
+        return view;
     }
 
 }
